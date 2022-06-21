27087b34fee47beb7c1c65f8d45c88d36ead9674
Christopher <christopher@ip-192-168-0-152.eu-west-2.compute.internal>
2022-06-21 15:26:20 +0100

    Episode 1

---
+++ src/Another.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Another(props) {
+    return (
+        <div>
+            Another Component {props.name}
+        </div>
+    );
+}
+
+export default Another;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Another from './Another';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function decrement(){
+    setCount(prevCount => prevCount - 1);
+  }
+  function increment(){
+    setCount(prevCount => prevCount + 1);
+  }
   return (
     <div className="App">
+      
       <header className="App-header">
+        <Another name="Chris" />
+        <div>
+          <span>{count}</span>
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{3 + 2}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
