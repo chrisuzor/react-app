6764ac6419b03e8a20093934fd6bf01780eb9452
Christopher <christopher@ADMINISTRATORs-MacBook-Pro-2.local>
2022-06-27 07:46:48 +0100

    Episode 10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,134 +4,23 @@ import NoTodos from './NoTodos';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 import useLocalStorage from '../hooks/useLocalStorage';
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { TodosContext } from '../context/TodosContext';
 
 function App() {
     const [name, setName] = useLocalStorage('name', '');
     const nameInputEl = useRef(null);
     const [todos, setTodos] = useLocalStorage('todos', []);
-    // const [todos, setTodos] = useState([
-    //    {
-    //     id: 1,
-    //     title: 'Finish React Series',
-    //     isComplete: false,
-    //     isEditing: false
-    //    },
-    //    {
-    //     id: 2,
-    //     title: 'Go Grocery',
-    //     isComplete: true,
-    //     isEditing: false
-    //    },
-    //    {
-    //     id: 3,
-    //     title: 'Take over world',
-    //     isComplete: false,
-    //     isEditing: false
-    //    },
-    // ]);
-
-    
     const [idForTodo, setIdForTodo] = useLocalStorage('idForTodo', 1);
+    const [filter, setFilter] = useState('all');
+    
 
-
-    function addTodo(todo){
-       
-        setTodos([
-            ...todos,
-            {
-            id: idForTodo,
-            title: todo,
-            isComplete: false,
-            isEditing: false
-        }])
-
-        setIdForTodo(prevIdForTodo => prevIdForTodo + 1);
-    }
-
-    function deleteTodo(id){
-        setTodos([...todos].filter(todo => todo.id !== id));
-    }
-
-    function completeTodo(id){
-       const updatedTodos = todos.map(todo => {
-            if (todo.id === id){
-                todo.isComplete = !todo.isComplete
-            }
-            
-            return todo;
-       });
-
-       setTodos(updatedTodos);
-    }
-
-    function updateTodo(event, id){
-        const updatedTodos = todos.map(todo => {
-             if (todo.id === id){
-                if (event.target.value.trim().length === 0){
-                    todo.isEditing = false
-                    return todo;
-                }
-                 todo.title = event.target.value
-                 todo.isEditing = false
-             }
-             
-             return todo;
-        });
- 
-        setTodos(updatedTodos);
-     }
-
-    function markAsEditing(id){
-        const updatedTodos = todos.map(todo => {
-            if (todo.id === id){
-                todo.isEditing = true
-            }
-            
-            return todo;
-       });
-
-       setTodos(updatedTodos);
-    }
-
-    function cancelEditing(event, id){
-        const updatedTodos = todos.map(todo => {
-            if (todo.id === id){
-                todo.isEditing = false
-            }
-            
-            return todo;
-       });
-       setTodos(updatedTodos);
-    }
-
-    function remainingCalculation(){
-        return todos.filter(todo => !todo.isComplete).length;
-    }
-
-    const remaining = useMemo(remainingCalculation, [todos]);
-
-    function clearCompleted(){
-        setTodos([...todos].filter(todo => !todo.isComplete));
-    }
-
-    function completeAllTodos(){
-        const updatedTodos = todos.map(todo => {
-            todo.isComplete = true;
-
-            return todo;
-        });
-
-        setTodos(updatedTodos);
-    }
-
-    function todosFiltered(filter){
+    function todosFiltered(){
         if (filter === 'active'){
             return todos.filter( todo => !todo.isComplete);
         }else if(filter === 'completed'){
             return todos.filter( todo => todo.isComplete);
         }
-
         return todos;
     }
 
@@ -148,6 +37,14 @@ function App() {
     }
 
   return (
+      <TodosContext.Provider value={{todos,
+       setTodos,
+        idForTodo,
+        setIdForTodo,
+         todosFiltered,
+         filter,
+         setFilter
+         }}>
     <div className="todo-app-container">
       <div className="todo-app">
         <div className="name-container">
@@ -165,26 +62,17 @@ function App() {
         </div>
 
         <h2>Todo App </h2>
-        <TodoForm addTodo={addTodo} />
+        <TodoForm />
 
         {todos.length > 0 ? (
-        <TodoList todos={todos}
-            completeTodo={completeTodo}
-            markAsEditing={markAsEditing}
-            updateTodo={updateTodo}
-            cancelEditing={cancelEditing}
-            deleteTodo={deleteTodo}
-            remaining={remaining}
-            clearCompleted={clearCompleted}
-            completeAllTodos={completeAllTodos}
-            todosFiltered={todosFiltered}
-        />
+        <TodoList />
         ): (
          <NoTodos />
         )
         }
       </div>
     </div>
+    </TodosContext.Provider>
   );
 }
 
--- src/components/TodoCompleteAll.jsx
+++ src/components/TodoCompleteAll.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import { TodosContext } from '../context/TodosContext';
 
 
-TodoCompleteAll.propTypes = {
-  completeAllTodos: PropTypes.func.isRequired
-};
 
-function TodoCompleteAll(props) {
+function TodoCompleteAll() {
+
+    const {todos, setTodos} = useContext(TodosContext);
+
+    function completeAllTodos(){
+      const updatedTodos = todos.map(todo => {
+        todo.isComplete = true;
+
+        return todo;
+    });
+
+    setTodos(updatedTodos);
+    }
+   
+
   return (
-    <div onClick={props.completeAllTodos} className="button">Check All</div>
+    <div onClick={completeAllTodos} className="button">Check All</div>
   )
 }
 
